1486a5b3c99945edc4a1c3225888d0256f34fba4
Raj Kamal <rrajkamal1999@gmail.com>
2024-04-17 11:57:21 +0530

    create heder section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,17 @@ import SignUp from './Pages/SignUp';
 import SignIn from './Pages/SignIn';
 import Dashboard from './Pages/Dashboard';
 import Blog from './Pages/Blog';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/blog" element={<Blog/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
