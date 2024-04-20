664ce364a9aeebee6dbf2b4c311e5c95e3ac59c3
Raj Kamal <rrajkamal1999@gmail.com>
2024-04-20 17:50:15 +0530

    add functionality to signup page

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,58 +1,93 @@
-import { Button, Label, TextInput } from "flowbite-react"
-import { Link } from "react-router-dom"
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+
+  const [formData, setFormData]=useState({})
+  const [errorMessage,setErrorMessage]=useState(null)
+  const [isLoading,setIsLoading] = useState(false);
+  const  navigate=useNavigate();
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()})
+  }
+
+  const handleSubmit=async (e) =>{
+    e.preventDefault();
+    if(!formData.username || !formData.password || !formData.email){
+      return setErrorMessage("All fields are required.")
+    }
+    try {
+      setIsLoading(true);
+      setErrorMessage(null);
+      const res = await  fetch('/api/auth/signup', {
+        method: 'POST',
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+        return  setErrorMessage(data.message)
+        
+      }
+      setIsLoading(false);
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setIsLoading(false);
+    }
+  }
   return (
     <div className="min-h-screen mt-20">
-    <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-    <div className="flex-1">
-    <Link to="/" className="font-bold dark:text-white text-6xl ">
-        <span className="px-2 py-1 bg-cyan-800 rounded-lg">Pets</span>
-        Blog
-      </Link>
-      <p className="text-sm mt-5 ">
-        Don’t have an account? Join
-      </p>
-    </div>
-    <div className="flex-1">
-    <form className="flex flex-col gap-4">
-      <div className="">
-      <Label value='Your username'/>
-      <TextInput 
-        type="text"
-        placeholder="Username"
-        id="username"
-      />
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-6xl ">
+            <span className="px-2 py-1 bg-cyan-800 rounded-lg">Pets</span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5 ">Don’t have an account? Join</p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div className="">
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" onChange={handleChange}/>
+            </div>
+            <div className="">
+              <Label value="Your email" />
+              <TextInput type="email" placeholder="Email" id="email"  onChange={handleChange}/>
+            </div>
+            <div className="">
+              <Label value="Your password" />
+              <TextInput type="password" placeholder="Password" id="password" onChange={handleChange} />
+            </div>
+            <Button type="submit" disabled={isLoading}>
+            {
+              isLoading ? (<>
+                <Spinner size='sm' />
+                <span className='pl-3'> Loading...</span>
+              </>
+              ):(
+                'Sign Up'
+                )
+            }
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in">Sign In</Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className="mt-5" color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
       </div>
-      <div className="">
-      <Label value='Your email'/>
-      <TextInput 
-        type="text"
-        placeholder="Email"
-        id="email"
-      />
-      </div>
-      <div className="">
-      <Label value='Your password'/>
-      <TextInput 
-        type="text"
-        placeholder="Password"
-        id="password"
-      />
-      </div>
-      <Button type="submit">
-        Sign Up
-      </Button>
-    </form>
-    <div className="flex gap-2 text-sm mt-5">
-      <span>Have an account?</span>
-      <Link to='/sign-in'>
-        Sign In
-      </Link>
     </div>
-    </div>
-    </div>
-
-    </div>
-  )
+  );
 }
