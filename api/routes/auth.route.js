c7c2b7abfe1f7f965a88e078f7705ad8ad4dcd26
Raj Kamal <rrajkamal1999@gmail.com>
2024-04-21 10:28:59 +0530

    create signin api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from  'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup,signin } from '../controllers/auth.controller.js';
+
 
 const router =express.Router();
 
